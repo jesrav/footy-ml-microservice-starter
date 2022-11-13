c975d74fd0d124c2adca0c8152ab613b2c712104
Jes Ravnbøl <jra@kapacity.dk>
2022-11-13 16:48:55 +0100

    wip

--- main.py
+++ main.py
@@ -21,25 +21,42 @@ with open(f"{MODEL_TRAINING_OUT_DIR}/model.pickle", "rb") as f:
     model = pickle.load(f)
 
 
-####################################################################################################
+#############################################################################
 # Examples of rule based examples that does not use any machine learning.
-####################################################################################################
-def predict_goal_diff_jesus_rule(result_to_predict: RowForML) -> int:
-    """Predict the goal diff off the game using only the row of features for the actual game.
-
-    If Jesus (user id 1) is on offence, his team will always win by 5
-    Otherwise we predict a random goal diff between -3 and 3, but never 0.
+#############################################################################
+def jesus_rule_prediction(result_to_predict: RowForML) -> float:
+    """Predict that Jesus (User id 1) always wins by 5 if he is on offence.
+    Otherwise, guess random goal diff between -3 and 3, but never 0.
     """
-
     if result_to_predict.team1_attacker_user_id == 1:
-        return 5
+        return 5.0
     elif result_to_predict.team2_attacker_user_id == 1:
-        return -5
+        return -5.0
     else:
-        return choice([-3, -2, -1, 1, 2, 3])
+        return choice([-3.0, -2.0, -1.0, 1.0, 2.0, 3.0])
 
 
-@app.post("/jesus_rule_predict")
-def predict(body: DataForML) -> int:
+def baseline_bob_prediction(result_to_predict: RowForML) -> float:
+    """Always predict a goal difference of 0"""
+    return 0.0
+
+
+@app.post("/jesus_rules/predict")
+def predict(body: DataForML) -> float:
     result_to_predict = [r for r in body.data if r.result_to_predict][0]
-    return predict_goal_diff_jesus_rule(result_to_predict)
+    return jesus_rule_prediction(result_to_predict)
+
+
+@app.post("/baseline_bob/predict")
+def predict(body: DataForML) -> float:
+    result_to_predict = [r for r in body.data if r.result_to_predict][0]
+    return jesus_rule_prediction(result_to_predict)
+
+
+#############################################################################
+# Example of using a trained ML model
+#############################################################################
+@app.post("/baseline_bob/predict")
+def predict(body: DataForML) -> float:
+    result_to_predict = [r for r in body.data if r.result_to_predict][0]
+    return jesus_rule_prediction(result_to_predict)
